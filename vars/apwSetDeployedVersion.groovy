3bc774eccabb8731226987bd730db2181598ae9a
Julien <julien@apwide.com>
2021-01-05 17:23:54 +0100

    Added support for buildNumber, description and attributes on setdeployedversion step

--- vars/apwSetDeployedVersion.groovy
+++ vars/apwSetDeployedVersion.groovy
@@ -6,6 +6,13 @@ import static com.apwide.jenkins.util.Utilities.executeStep
 
 def call(Map config = null) {
     executeStep(this, config) { ScriptWrapper script, Parameters parameters ->
-        return new Environment(script, parameters).setDeployedVersion(parameters.application, parameters.category, parameters.params.version)
+        return new Environment(script, parameters).setDeployedVersion(
+                parameters.application,
+                parameters.category,
+                parameters.params.version,
+                parameters.params.buildNumber,
+                parameters.params.description,
+                parameters.params.attributes
+        )
     }
 }
